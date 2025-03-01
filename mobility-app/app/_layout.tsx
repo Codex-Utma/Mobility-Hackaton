279aaf60dcf4d5b51d9b6b90952e58e6378f6a9f
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-01 14:05:13 -0600

    feat: set up auth0 provider

--- mobility-app/app/_layout.tsx
+++ mobility-app/app/_layout.tsx
@@ -1,5 +1,19 @@
+import { Auth0Provider} from 'react-native-auth0';
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Auth0Provider domain={`${process.env.EXPO_PUBLIC_AUTH0_DOMAIN}`} clientId={`${process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID}`}>
+      <SafeAreaProvider>
+        <StatusBar />
+        <Stack
+          screenOptions={{
+            headerShown: false
+          }}
+        />
+      </SafeAreaProvider>
+    </Auth0Provider>
+  );
 }
