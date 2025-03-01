c365acf4d1de260dd90c3aea8b334c9391f3293e
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-01 16:13:01 -0600

    feat: add text to last uses

--- mobility-app/components/Mobile/Card/CardLastUses.tsx
+++ mobility-app/components/Mobile/Card/CardLastUses.tsx
@@ -1,9 +1,11 @@
 import { LastUsedType } from "@/assets/types/Card";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
-export default function CardLastUses({lastUsesMocks}: {lastUsesMocks: LastUsedType[]}) {
-    return (
-        <FlatList
+export default function CardLastUses({ lastUsesMocks }: { lastUsesMocks: LastUsedType[] }) {
+  return (
+    <View>
+      <Text style={{ fontSize: 18, marginVertical: 12 }}>Últimos usos</Text>
+      <FlatList
         data={lastUsesMocks}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
@@ -14,24 +16,25 @@ export default function CardLastUses({lastUsesMocks}: {lastUsesMocks: LastUsedTy
           </View>
         )}
       />
-    )
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    listItem: {
-        width: 320,
-        padding: 12,
-        backgroundColor: "#f9f9f9",
-        borderRadius: 8,
-        marginBottom: 8,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation: 2,
-      },
-      listText: {
-        fontSize: 14,
-        color: "black",
-      }
+  listItem: {
+    width: 320,
+    padding: 12,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    marginBottom: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  listText: {
+    fontSize: 14,
+    color: "black",
+  }
 })
