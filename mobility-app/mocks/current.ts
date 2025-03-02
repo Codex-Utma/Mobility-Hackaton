10978d9d4f5f3627ea16a90d85e26f88a9af3269
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-01 21:19:50 -0600

    feat: add test data for current bus

---
+++ mobility-app/mocks/current.ts
@@ -0,0 +1,7 @@
+import { CurrentDataType } from "@/types/Current";
+
+export const currentData: CurrentDataType = {
+    route: 1,
+    cost: 10.50,
+    time: "14:20",
+}
---
+++ mobility-app/types/Current.ts
@@ -0,0 +1,5 @@
+export interface CurrentDataType {
+    route: number;
+    cost: number;
+    time: string;
+}
