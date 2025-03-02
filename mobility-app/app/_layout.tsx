90d1ea93b7b23c405e7ec0065478288a7e795904
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-02 13:27:56 -0600

    feat: require camera permissions

--- mobility-app/app/_layout.tsx
+++ mobility-app/app/_layout.tsx
@@ -3,7 +3,30 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { useCameraPermissions } from "expo-camera";
+import { Button, StyleSheet, Text, View } from 'react-native';
+
 export default function RootLayout() {
+
+  const [permission, setPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return (
+      <View style={styles.centeredContainer}>
+        <Text>No permission</Text>
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={setPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
   return (
     <Auth0Provider domain={`${process.env.EXPO_PUBLIC_AUTH0_DOMAIN}`} clientId={`${process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID}`}>
       <SafeAreaProvider>
@@ -17,3 +40,19 @@ export default function RootLayout() {
     </Auth0Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
